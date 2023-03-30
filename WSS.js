460e3b09639c807260212c64d68ec8ebffc73759
jqsx <101830653+jqsx@users.noreply.github.com>
2023-03-30 10:56:23 +0200

    mmmmmmmm

--- WSS.js
+++ WSS.js
@@ -3,12 +3,13 @@ const { WebSocketServer } = require('ws');;
 export default class wss extends WebSocketServer {
     constructor(options) {
         super(options);
+        this.register();
     }
 
     register() {
         this.on('connection', (ws, req) => {
             ws.on('open', () => {
-                
+
             })
         })
     }
